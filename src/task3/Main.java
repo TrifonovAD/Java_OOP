c5b73c87b19ba6b146b6ab06f9a2e48a64e9483b
TrifonovAD <trifonov76@gmail.com>
2023-04-21 12:00:37 +0300

    End working seminar 3

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -1,3 +1,11 @@
+//- Создать класс Студент
+//- Создать класс УчебнаяГруппаИтератор
+//- Создать класс УчебнаяГруппаИтератор, заставив его реализовать интерфейс Iterator
+//- Реализовать его контракты (включая удаление)
+
+//- Создать класс СтудентКомпаратор реализующий интерфейс Comparator
+//- Реализовать контракт compare () со сравнением по какому-либо параметру (пример: сочетание Имя+Фамилия)
+
 package task3;
 
 import java.util.Arrays;
@@ -7,16 +15,21 @@ public class Main {
     public static void main(String[] args) {
         StudentsGroup students = new StudentsGroup();
         students.setStudents(Arrays.asList(
-                new Student("Alexey", "Smirnov", 1),
-                new Student("Sergey", "Ivanov", 2),
-                new Student("Ivan", "Petrov", 3),
-                new Student("Anna", "Karenina", 1),
-                new Student("Olga", "Simonova", 2),
-                new Student("Svetlana", "Budanova", 3)
+                new Student("Alexey", "Smirnov", 1, 4.1),
+                new Student("Sergey", "Ivanov", 2, 4.3),
+                new Student("Ivan", "Petrov", 3, 3.8),
+                new Student("Anna", "Karenina", 1, 4.1),
+                new Student("Olga", "Simonova", 2, 3.4),
+                new Student("Svetlana", "Budanova", 3, 5.0)
         ));
         Iterator<Student> iter = students.iterator();
         while (iter.hasNext()) {
             System.out.println(iter.next());
+        }
+        System.out.println("\n=======Отсортированный список=======");
+        students.getStudents().sort(new StudentComparator());
+        for (Student student : students) {
+            System.out.println(student);
         }
     }
 }
--- src/task3/Student.java
+++ src/task3/Student.java
@@ -4,15 +4,33 @@ public class Student {
     private String firstName;
     private String lastName;
     private Integer groupId;
+    private Double averageScore;
 
-    public Student(String firstName, String lastName, Integer gorupId) {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public Double getAverageScore() {
+        return averageScore;
+    }
+
+    public Student(String firstName, String lastName, Integer groupId, Double averageScore) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.groupId = gorupId;
+        this.groupId = groupId;
+        this.averageScore = averageScore;
     }
 
     @Override
     public String toString() {
-        return String.format("%s %s. Group: %d", lastName, firstName, groupId);
+        return String.format("%s %s. Group: %d. Average score: %.2f", lastName, firstName, groupId, averageScore);
     }
 }
