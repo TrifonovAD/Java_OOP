b01e47afeba9be4104dbc6835308c4b3af3c660f
TrifonovAD <trifonov76@gmail.com>
2023-05-05 19:07:43 +0300

    Homework 6 initial commit

---
+++ src/homework6/model/HotDrink.java
@@ -0,0 +1,20 @@
+package homework6.model;
+
+public class HotDrink extends Drink {
+    private int temperature;
+
+
+    public HotDrink(String name, int cost, Double volume, int temperature) {
+        super(name, cost, volume);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s. Temperature %d", super.toString(), temperature);
+    }
+}
